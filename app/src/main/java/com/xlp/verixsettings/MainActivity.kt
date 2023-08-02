0a9c555bff77e7b5ce62552c228e05bea0772d4e
XPH <2262519065@qq.com>
2023-08-02 14:49:53 +0800

    初始化仓库(2)

---
+++ app/src/main/java/com/xlp/verixsettings/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xlp.verixsettings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.xlp.verixsettings.R.layout.vibration_function
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(vibration_function)
+    }
+}
