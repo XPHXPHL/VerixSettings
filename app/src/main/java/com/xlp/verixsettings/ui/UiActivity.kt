128f7d3c7d4b052d9c330f70d91e2ba918991c35
XPH <2262519065@qq.com>
2023-08-08 03:21:32 +0800

    app:添加时间显秒功能

--- app/src/main/java/com/xlp/verixsettings/ui/UiActivity.kt
+++ app/src/main/java/com/xlp/verixsettings/ui/UiActivity.kt
@@ -1,22 +1,39 @@
 package com.xlp.verixsettings.ui
 
 import androidx.fragment.app.Fragment
+import androidx.preference.Preference
 import com.xlp.verixsettings.R
 import com.xlp.verixsettings.ui.base.BaseAppCompatActivity
 import com.xlp.verixsettings.ui.base.BasePreferenceFragment
+import com.xlp.verixsettings.utils.execShell
 
-class UiActivity : BaseAppCompatActivity() {
+class UiActivity : BaseAppCompatActivity(){
 
     override fun initFragment(): Fragment {
         setTitle(R.string.Ui)
         return PageFragment()
     }
 
-    class PageFragment : BasePreferenceFragment() {
+    class PageFragment : BasePreferenceFragment(), Preference.OnPreferenceChangeListener {
+        private var mClockSeconds: Preference? = null
+        override fun initPrefs() {
+            mClockSeconds = findPreference("clock_seconds")
+            mClockSeconds?.onPreferenceChangeListener = this
+        }
+
         override fun getContentResId(): Int {
             return R.xml.prefs_ui
         }
 
-        override fun initPrefs() {}
+        override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+            if (preference === mClockSeconds) {
+                if (newValue as Boolean) {
+                    execShell("settings put secure clock_seconds 1")
+                } else {
+                    execShell("settings put secure clock_seconds 0")
+                }
+            }
+            return true
+        }
     }
-}
+    }
