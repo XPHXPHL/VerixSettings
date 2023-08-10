82cdb8a799ca91527a85089477f51f962d94bc9e
XPH <226251906@qq>
2023-08-11 04:42:41 +0800

    app:添加桌面隐藏图标功能（没搞定

--- app/src/main/java/com/xlp/verixsettings/ui/OtherActivity.kt
+++ app/src/main/java/com/xlp/verixsettings/ui/OtherActivity.kt
@@ -1,6 +1,9 @@
 package com.xlp.verixsettings.ui
 
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
+import androidx.preference.Preference
 import com.xlp.verixsettings.R
 import com.xlp.verixsettings.ui.base.BaseAppCompatActivity
 import com.xlp.verixsettings.ui.base.BasePreferenceFragment
@@ -12,11 +15,37 @@ class OtherActivity : BaseAppCompatActivity() {
         return PageFragment()
     }
 
-    class PageFragment : BasePreferenceFragment() {
+
+     class PageFragment : BasePreferenceFragment(), Preference.OnPreferenceChangeListener {
+        private var mHideIcon: Preference? = null
+        override fun initPrefs() {
+            mHideIcon = findPreference("hide_icon")
+            mHideIcon?.onPreferenceChangeListener = this
+        }
+
         override fun getContentResId(): Int {
             return R.xml.prefs_other
         }
-
-        override fun initPrefs() {}
+        override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+            val context = requireContext().applicationContext
+            val pm = context.packageManager
+            val componentName = ComponentName(context,"com.xlp.verixsettings.ui.MainActivity")
+            if (preference === mHideIcon) {
+                if (newValue as Boolean) {
+                    pm.setComponentEnabledSetting(
+                        componentName,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP
+                    )
+                } else {
+                    pm.setComponentEnabledSetting(
+                        componentName,
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP
+                    )
+                }
+            }
+            return true
+        }
     }
 }
