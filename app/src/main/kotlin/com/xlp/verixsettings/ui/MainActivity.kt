f2efed6121ef1fa3d2102499f3100a5f312452b9
XPH <2262519065@qq.com>
2023-08-18 15:11:18 +0800

    app:Update CheckedModel function

--- app/src/main/kotlin/com/xlp/verixsettings/ui/MainActivity.kt
+++ app/src/main/kotlin/com/xlp/verixsettings/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import cn.fkj233.ui.activity.MIUIActivity
 import cn.fkj233.ui.dialog.MIUIDialog
 import com.xlp.verixsettings.R
+import com.xlp.verixsettings.R.string.not_support_device
 import com.xlp.verixsettings.ui.pages.AndroidPage
 import com.xlp.verixsettings.ui.pages.AppInstallPage
 import com.xlp.verixsettings.ui.pages.ChargePage
@@ -14,15 +15,30 @@ import com.xlp.verixsettings.ui.pages.SettingsPage
 import com.xlp.verixsettings.ui.pages.SystemUiPage
 import com.xlp.verixsettings.ui.pages.VibratorPage
 import com.xlp.verixsettings.utils.BackupUtils
+import com.xlp.verixsettings.utils.CheckedModel.checked
 import kotlin.system.exitProcess
 
 
 class MainActivity : MIUIActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         checkLSPosed()
+        checkModel()
         super.onCreate(savedInstanceState)
     }
 
+    private fun checkModel(){
+        val checkState = checked()
+        if (!checkState){
+            MIUIDialog(this) {
+                setTitle(R.string.tips)
+                setMessage(not_support_device)
+                setCancelable(false)
+                setRButton(R.string.done) {
+                    exitProcess(0)
+                }
+            }.show()
+        }
+    }
     @Suppress("DEPRECATION")
     @SuppressLint("WorldReadableFiles")
     private fun checkLSPosed() {
--- app/src/main/kotlin/com/xlp/verixsettings/utils/CheckedModel.kt
+++ app/src/main/kotlin/com/xlp/verixsettings/utils/CheckedModel.kt
@@ -4,30 +4,32 @@ import java.util.Locale
 
 object CheckedModel {
     fun checked(): Boolean {
-        val meizuModel = getProp("ro.product.vendor.brand")
-        when (getProp("ro.product.vendor.name")) {
+        val meizuModel = getProp("ro.p" + "ro" + "du" + "ct.v" + "en" + "do" + "r.br" + "and")
+        when (getProp("ro.p" + "ro" + "du" + "ct.v" + "end" + "or.na" + "me")) {
+            //Sm8350
+            "fu" + "x" + "i" -> return true
             //Sm8250
-            "apollo" -> return true
-            "munch" -> return true
-            "alioth" -> return true
-            "cas" -> return true
-            "cmi" -> return true
-            "umi" -> return true
+            "ap" + "ol" + "lo" -> return true
+            "mu" + "nch" -> return true
+            "al" + "io" + "th" -> return true
+            "c" + "a" + "s" -> return true
+            "c" + "m" + "i" -> return true
+            "u" + "mi" -> return true
             //Sm8150
-            "cepheus" -> return true
-            "raphael" -> return true
+            "ce" + "ph" + "e" + "us" -> return true
+            "r" + "a" + "p" + "h" + "a" + "e" + "l" -> return true
             //SDM845
-            "perseus" -> return true
-            "polaris" -> return true
+            "per" + "se" + "us" -> return true
+            "po" + "la" + "ri" + "s" -> return true
             //Other
-            "picasso" -> return true
-            "phoenix" -> return true
-
+            "pi" + "ca" + "ss" + "o" -> return true
+            "ph" + "oe" + "ni" + "x" -> return true
         }
         when (meizuModel.lowercase(Locale.ROOT)) {
-            "meizu" -> return true
-            "sony" -> return true
-            "lenovo" -> return true
+            "me" + "i" + "zu" -> return true
+            "s" + "o" + "ny" -> return true
+            "len" + "ovo" -> return true
+            "One" + "Plus" -> return true
         }
         return false
     }
