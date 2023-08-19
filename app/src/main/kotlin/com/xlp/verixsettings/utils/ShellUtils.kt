db781a43fdcd83c14950f9e8c18b1f7fbb04d403
XPH <2262519065@qq.com>
2023-08-19 12:14:41 +0800

    add:ShellUtils.kt

---
+++ app/src/main/kotlin/com/xlp/verixsettings/utils/ShellUtils.kt
@@ -0,0 +1,100 @@
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+
+object ShellUtils {
+
+    const val COMMAND_SU = "su"
+    const val COMMAND_SH = "sh"
+    const val COMMAND_EXIT = "exit\n"
+    const val COMMAND_LINE_END = "\n"
+
+    class CommandResult(val result: Int, val successMsg: String?, val errorMsg: String?)
+
+    fun checkRootPermission(): Boolean {
+        return execCommand("echo root", true, false).result == 0
+    }
+
+    fun execCommand(command: String, isRoot: Boolean): CommandResult {
+        return execCommand(arrayOf(command), isRoot, true)
+    }
+
+    fun execCommand(commands: List<String>?, isRoot: Boolean): CommandResult {
+        return execCommand(commands?.toTypedArray(), isRoot, true)
+    }
+
+    fun execCommand(commands: Array<String>?, isRoot: Boolean): CommandResult {
+        return execCommand(commands, isRoot, true)
+    }
+
+    fun execCommand(command: String, isRoot: Boolean, isNeedResultMsg: Boolean): CommandResult {
+        return execCommand(arrayOf(command), isRoot, isNeedResultMsg)
+    }
+
+    fun execCommand(commands: List<String>?, isRoot: Boolean, isNeedResultMsg: Boolean): CommandResult {
+        return execCommand(commands?.toTypedArray(), isRoot, isNeedResultMsg)
+    }
+
+    fun execCommand(commands: Array<String>?, isRoot: Boolean, isNeedResultMsg: Boolean): CommandResult {
+        var result = -1
+        if (commands == null || commands.isEmpty()) {
+            return CommandResult(result, null, null)
+        }
+
+        var process: Process? = null
+        var successResult: BufferedReader? = null
+        var errorResult: BufferedReader? = null
+        var successMsg: StringBuilder? = null
+        var errorMsg: StringBuilder? = null
+
+        var os: DataOutputStream? = null
+        try {
+            process = Runtime.getRuntime().exec(if (isRoot) COMMAND_SU else COMMAND_SH)
+            os = DataOutputStream(process.outputStream)
+            for (command in commands) {
+                if (command == null) {
+                    continue
+                }
+
+                // don't use os.writeBytes(commmand), avoid chinese charset error
+                os.write(command.toByteArray())
+                os.writeBytes(COMMAND_LINE_END)
+                os.flush()
+            }
+            os.writeBytes(COMMAND_EXIT)
+            os.flush()
+
+            result = process.waitFor()
+            // get command result
+            if (isNeedResultMsg) {
+                successMsg = StringBuilder()
+                errorMsg = StringBuilder()
+                successResult = BufferedReader(InputStreamReader(process.inputStream))
+                errorResult = BufferedReader(InputStreamReader(process.errorStream))
+                var s: String?
+                while (successResult.readLine().also { s = it } != null) {
+                    successMsg.append(s)
+                }
+                while (errorResult.readLine().also { s = it } != null) {
+                    errorMsg.append(s)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                os?.close()
+                successResult?.close()
+                errorResult?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            process?.destroy()
+        }
+        return CommandResult(result, successMsg?.toString(), errorMsg?.toString())
+    }
+}
