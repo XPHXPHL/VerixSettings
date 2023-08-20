9c66f169f1bd1b14475cd5c83e2f2f315c924b48
XPH <2262519065@qq.com>
2023-08-20 23:06:09 +0800

    app:添加关于手机状态 1.内核版本 2.处理器代号 3.电池设计容量 4.电池实际充满容量

--- app/src/main/kotlin/com/xlp/verixsettings/activity/pages/EggsPages.kt
+++ app/src/main/kotlin/com/xlp/verixsettings/activity/pages/EggsPages.kt
@@ -6,6 +6,7 @@ import cn.fkj233.ui.activity.MIUIActivity.Companion.context
 import cn.fkj233.ui.activity.annotation.BMMenuPage
 import cn.fkj233.ui.activity.data.BasePage
 import com.xlp.verixsettings.BuildConfig
+import com.xlp.verixsettings.R
 import com.xlp.verixsettings.R.drawable.about_creamdraft
 import com.xlp.verixsettings.R.drawable.about_ex3124
 import com.xlp.verixsettings.R.drawable.about_xph
@@ -13,13 +14,17 @@ import com.xlp.verixsettings.R.drawable.about_yukonga
 import com.xlp.verixsettings.R.string.about_module
 import com.xlp.verixsettings.R.string.about_us
 import com.xlp.verixsettings.R.string.eggs_tips
+import com.xlp.verixsettings.R.string.kernel_version
 import com.xlp.verixsettings.R.string.module_build_time
 import com.xlp.verixsettings.R.string.module_version
+import com.xlp.verixsettings.R.string.phone_state
+import com.xlp.verixsettings.R.string.soc_chip
+import com.xlp.verixsettings.utils.execShell
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 @BMMenuPage
-class EggsPages(): BasePage() {
+class EggsPages : BasePage() {
     override fun getTitle(): String {
         setTitle(getString(eggs_tips))
         return getString(eggs_tips)
@@ -68,6 +73,23 @@ class EggsPages(): BasePage() {
         TextSummary(textId = module_version, tips = "${BuildConfig.VERSION_NAME}-${BuildConfig.BUILD_TYPE}")
         val buildTime = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(BuildConfig.BUILD_TIME)
         TextSummary(textId = module_build_time, tips = buildTime)
-        Text()
+        Line()
+        TitleText(textId = phone_state)
+        val batteryDesign = execShell("cat /sys/class/power_supply/battery/charge_full_design | awk '{print int(\$1/1000)}'")
+        val batteryFull = execShell("cat /sys/class/power_supply/battery/charge_full | awk '{print int(\$1/1000)}'")
+        val kernelVersion = execShell("cat /proc/version")
+        val socChip = execShell("cat /sys/devices/soc0/chip_name")
+        TextSummary(
+            textId = kernel_version, tips = kernelVersion
+        )
+        TextSummary(
+            textId = soc_chip, tips = socChip
+        )
+        TextSummary(
+            textId = R.string.battery_design, tips = batteryDesign
+        )
+        TextSummary(
+            textId = R.string.battery_full, tips = batteryFull
+        )
     }
 }
