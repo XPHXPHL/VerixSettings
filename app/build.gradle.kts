c513f98e86ec33ca2503af70900aca5715cfde1c
XPH <2262519065@qq.com>
2023-08-29 19:39:56 +0800

    Update build.gradle.kts Form YuKongA

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,49 +1,69 @@
 @file:Suppress("UnstableApiUsage")
 
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import java.io.ByteArrayOutputStream
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
 }
-
 android {
     val buildTime = System.currentTimeMillis()
-    compileSdk = 33
+    compileSdk = 34
     namespace = "com.xlp.verixsettings"
     defaultConfig {
         applicationId = namespace
         minSdk = 33
-        targetSdk = 33
-        versionCode = 114514
-        versionName = "1.8"
+        targetSdk = 34
+        versionCode = getVersionCode()
+        versionName = "1.8" + getVersionName()
         buildConfigField("long", "BUILD_TIME", "$buildTime")
-        vectorDrawables {
-            useSupportLibrary = true
+        //noinspection ChromeOsAbiSupport
+        ndk.abiFilters += arrayOf("arm64-v8a")
+    }
+    val properties = Properties()
+    runCatching { properties.load(project.rootProject.file("local.properties").inputStream()) }
+    val keystorePath = properties.getProperty("KEYSTORE_PATH") ?: System.getenv("KEYSTORE_PATH")
+    val keystorePwd = properties.getProperty("KEYSTORE_PASS") ?: System.getenv("KEYSTORE_PASS")
+    val alias = properties.getProperty("KEY_ALIAS") ?: System.getenv("KEY_ALIAS")
+    val pwd = properties.getProperty("KEY_PASSWORD") ?: System.getenv("KEY_PASSWORD")
+    if (keystorePath != null) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(keystorePath)
+                storePassword = keystorePwd
+                keyAlias = alias
+                keyPassword = pwd
+                enableV3Signing = true
+            }
         }
-    }
-    buildFeatures {
-        buildConfig = true
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildTypes {
         release {
-            isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro","proguard-log.pro")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+                "proguard-log.pro"
+            )
+            if (keystorePath != null) signingConfig = signingConfigs.getByName("release")
         }
+        debug {
+            if (keystorePath != null) signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    androidResources {
+        additionalParameters += arrayOf("--allow-reserved-package-id", "--package-id", "0x67")
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    androidResources {
-        additionalParameters += arrayOf("--allow-reserved-package-id", "--package-id", "0x78")
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
     packaging {
         resources {
@@ -54,10 +74,42 @@ android {
         }
         applicationVariants.all {
             outputs.all {
-                (this as BaseVariantOutputImpl).outputFileName = "VerixSettings-$versionName-$name-$buildTime.apk"
+                (this as BaseVariantOutputImpl).outputFileName =
+                    "VerixSettings-$versionName($versionCode)-$name-$buildTime.apk"
             }
         }
     }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+fun getGitCommitCount(): Int {
+    val out = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-list", "--count", "HEAD")
+        standardOutput = out
+    }
+    return out.toString().trim().toInt()
+}
+
+fun getGitDescribe(): String {
+    val out = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "describe", "--tags", "--always")
+        standardOutput = out
+    }
+    return out.toString().trim()
+}
+
+fun getVersionCode(): Int {
+    val commitCount = getGitCommitCount()
+    val major = 5
+    return major + commitCount
+}
+
+fun getVersionName(): String {
+    return getGitDescribe()
 }
 
 kotlin {
@@ -67,10 +119,8 @@ kotlin {
         }
     }
 }
-
 dependencies {
     compileOnly(libs.xposed.api)
     implementation(libs.core.ktx)
     implementation(project("::blockmiui"))
 }
-
